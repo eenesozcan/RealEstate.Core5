ad68fe18262e3da13194021e8175f488209bcdd4
Enes OZCAN <enesozcan@hotmail.com>
2022-10-31 23:17:33 +0300

    Add project files.

---
+++ RealEstate.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstate.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-CSTSJL1\\MSSQLSERVER2019; initial catalog=DbCoreRealEstate; integrated security=true");
+        }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<About> Abouts { get; set; }
+    }
+}
