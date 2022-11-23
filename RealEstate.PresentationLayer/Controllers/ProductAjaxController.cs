801ffccdd5cc314456e901cebe69acdaba73e230
Enes OZCAN <enesozcan@hotmail.com>
2022-11-23 21:14:27 +0300

    23.11.2022

---
+++ RealEstate.PresentationLayer/Controllers/ProductAjaxController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate.DataAccessLayer.Concrete;
+using RealEstate.EntityLayer.Concrete;
+using System;
+using System.Linq;
+
+namespace RealEstate.PresentationLayer.Controllers
+{
+
+    [AllowAnonymous]
+    public class ProductAjaxController : Controller
+    {
+
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            var valuesJson = JsonConvert.SerializeObject(context.Products.ToList());
+            return Json(valuesJson);
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.AppUserID = 1;
+            p.CategoryID = 2;
+            context.Products.Add(p);
+            context.SaveChanges();
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+        }
+
+
+
+
+    }
+}
---
+++ RealEstate.PresentationLayer/Models/ProductReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+
+namespace RealEstate.PresentationLayer.Models
+{
+    public class ProductReportViewModel
+    {
+        public string ProductTitle { get; set; }
+        public string ProductPrice { get; set; }
+        public DateTime ProductDate { get; set; }
+    }
+}
