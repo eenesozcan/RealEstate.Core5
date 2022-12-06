d176b76b69b61c377783e482a09431af322d73a0
Enes OZCAN <enesozcan@hotmail.com>
2022-12-07 00:57:13 +0300

    30.11.2022

---
+++ RealEstate.PresentationLayer/Areas/Default/Controllers/AdvertController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.PresentationLayer.Areas.Default.Controllers
+{
+    [Area("Default")]
+    [AllowAnonymous]
+    public class AdvertController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate.PresentationLayer/ViewComponents/Adverts/_CommentListByAdvert.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.PresentationLayer.ViewComponents.Adverts
+{
+    public class _CommentListByAdvert : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
